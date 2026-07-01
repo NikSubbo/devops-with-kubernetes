1508a33eb4eb0b8b149a7cf5c7b92377a9a3507a
ns <nikolai.subbotin@gmail.com>
2026-07-01 22:56:32 +0300

    1.13.

--- project/index.js
+++ project/index.js
@@ -5,6 +5,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + "/public"));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
