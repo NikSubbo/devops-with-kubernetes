3f30e976b0bfc852d2e74da06316a6ea1aac5e97
ns <nikolai.subbotin@gmail.com>
2026-07-03 23:12:37 +0300

    2.2. v1

---
+++ project/todo-backend/index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/todos", async (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.post("/todos", async (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`);
+});
